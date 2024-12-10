9615fde36bef376bffa0dcefa586bda4e33c9001
EGEMERASCA <dudugemerasca@gmail.com>
2024-12-10 21:40:10 +0000

    start

---
+++ app/src/main/java/com/example/tipscalculator/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.tipscalculator
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+//valor total conta
+//numero de pessoas
+//porcentagem tips
+    //10%, 15% ou 20%
+// calcular / limpar
+
+//programar as views
+//recuperar as viewa do layout
+    //find view by id
+    //view binding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Camel case
+
+        val tvTitle: TextView = findViewById(R.id.tv_title)
+        val btnClean: Button = findViewById(R.id.btn_clean)
+
+        btnClean.setOnClickListener {
+            tvTitle.text = "Novo texto"
+        }
+
+    }
+}
