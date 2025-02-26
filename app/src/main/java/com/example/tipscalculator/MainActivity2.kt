3254a26262152c80fba0f6bc8bda21f9a6180458
EGEMERASCA <dudugemerasca@gmail.com>
2025-02-26 21:55:44 +0000

    ..

--- app/src/main/java/com/example/tipscalculator/MainActivity2.kt
+++ app/src/main/java/com/example/tipscalculator/MainActivity2.kt
@@ -2,7 +2,11 @@ package com.example.tipscalculator
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-
+/////////////////////////////
+// conectar a page quando done
+// mostrar resultado + descriçao da conta
+// opcao de voltar
+////////////////////////////
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
