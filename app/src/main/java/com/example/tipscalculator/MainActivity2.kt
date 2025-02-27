bf130918017f599115c2826a2b9fee69f9a9707b
EGEMERASCA <dudugemerasca@gmail.com>
2025-02-27 23:16:42 +0000

    ..

--- app/src/main/java/com/example/tipscalculator/MainActivity2.kt
+++ app/src/main/java/com/example/tipscalculator/MainActivity2.kt
@@ -2,6 +2,7 @@ package com.example.tipscalculator
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+
 /////////////////////////////
 // conectar a page quando done
 // recuperar valores
@@ -13,7 +14,11 @@ class MainActivity2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
+        val totalTable = intent.getFloatExtra("totalTable", 0.0f)
+        val numPeople = intent.getIntExtra("numPeople", 0)
+        val percentage = intent.getIntExtra("percentage", 0)
+        val totalAmount = intent.getFloatExtra("totalAmount", 0.0f)
 
-
+        println("Ai calica  " + totalAmount)
     }
 }
